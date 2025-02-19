9d151cb9535282741aa662038a3296ea5803d10e
wanjawischmeier <69650445+wanjawischmeier@users.noreply.github.com>
2025-02-19 18:42:37 +0100

    Update f.js

--- f.js
+++ f.js
@@ -1,11 +1,4 @@
-
-
 (() => {
-  function test() {
-    console.log("test");
-  }
-  
-  console.log("bookmarklet loaded");
     console.log("Script loaded and running!");
     // Your actual code here
 })();
